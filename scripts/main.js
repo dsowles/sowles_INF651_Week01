8a8c307797c8e4b946dcd7b33424bd01e6f69383
dsowles <d_sowles@mail.fhsu.edu>
2024-08-26 20:43:54 -0500

    Added var declaration

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+
+//Basic var declaration example
+let myVariable = "Bob";
+myVariable = "Steve";
+
+
+//Basic use of conditional statements
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("Yay, I love chocolate ice cream!");
+} else {
+  alert("Awwww, but chocolate is my favorite…");
+}
+
+
+//Function statement declaration and use example
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+  }
+
+  multiply(2,3)
+  multiply(4,6)
+  multiply(6.7)
+
+//javascript event example
+document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+});
+
+//example of event handling (lambda syntax)
+/*
+document.querySelector("html").addEventListener("click", () => {
+    alert("Ouch! Stop poking me!");
+  });
+*/
+
+
+  
